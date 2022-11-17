a47e31c87f6813b583ca2da11fe2132c9912c80f
ViniciusStanula <48488978+ViniciusStanula@users.noreply.github.com>
2022-11-17 13:25:27 -0300

    Add files via upload

--- serp-competitors.py
+++ serp-competitors.py
@@ -1,6 +1,20 @@
-pip install google -q
+import streamlit as st
 
-from googlesearch import search
-query = "analista de seo"
-for result in search(query, num=10, stop=10, lang="pt"):
-    print(result)
+st.sidebar.title('Know your Competitors')
+keyword = st.sidebar.text_input(label="Digite a Palavra-Chave", value="SEO", max_chars=None, key=None, help='A palavra-chave que gostaria de buscar os competidores. Apenas uma palavra por vez.')
+language = st.sidebar.selectbox('Escolha o Idioma da Busca',['Português', 'Inglês'])
+numberKeywords = st.sidebar.slider('Número de Competidores:', min_value=1, max_value=20)
+
+if language == 'Inglês':
+    language = 'en'
+elif language == 'Português':
+    language = 'pt-br'
+
+st.write('Aqui estão seus competidores:')
+
+if st.sidebar.button('Buscar Competidores'):
+    from googlesearch import search
+    for result in search(keyword, num=numberKeywords, stop=numberKeywords, lang=language):
+        st.markdown(result)
+
+st.sidebar.markdown('Script by **Vinicius Stanula**.')
